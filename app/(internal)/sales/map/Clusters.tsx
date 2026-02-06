54020f7705eceb9388fa09aff1aaad5181fb2ba7
unknown <edson.bigotires@gmail.com>
2026-02-06 16:38:39 -0600

    Fix: Create missing Heatmap and Clusters components

---
+++ app/(internal)/sales/map/Clusters.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Clusters() {
+  return (
+    <div className="w-full h-full bg-gray-100 flex items-center justify-center text-gray-400">
+      Clusters View (Coming Soon)
+    </div>
+  );
+}
---
+++ app/(internal)/sales/map/Heatmap.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Heatmap() {
+  return (
+    <div className="w-full h-full bg-gray-100 flex items-center justify-center text-gray-400">
+      Heatmap View (Coming Soon)
+    </div>
+  );
+}
