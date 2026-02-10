dbc03592bca8e8a4e42459e0431899da42e1ebf1
unknown <edson.bigotires@gmail.com>
2026-02-10 17:57:38 -0600

    Fix: Remove invalid customer prop from RequestsPanel

--- app/customer-management/[id]/page.tsx
+++ app/customer-management/[id]/page.tsx
@@ -209,7 +209,6 @@ export default function CustomerManagementPage({ params }: any) {
 
         {activeTab === "vehicles" && (
           <VehiclesPanel
-            // FIX: Removed invalid 'customer' prop
             customerId={customerId}
             onOpenLightbox={(imgs, index) => {
               setLightboxImages(imgs);
@@ -221,7 +220,7 @@ export default function CustomerManagementPage({ params }: any) {
 
         {activeTab === "requests" && (
           <RequestsPanel
-            customer={customer}
+            // FIX: Removed invalid 'customer' prop
             customerId={customerId}
             onOpenLightbox={(imgs, index) => {
               setLightboxImages(imgs);
